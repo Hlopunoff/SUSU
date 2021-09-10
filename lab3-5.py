1a28e8ec8d5c1a8ac822cb19603614441dcadb5b
Vasiliy <vasia22012003@gmail.com>
2021-09-10 18:30:27 +0500

    Third lab is done, but task 6 haven`t finished yet

---
+++ lab3-5.py
@@ -0,0 +1,9 @@
+from random import randint
+# name = input('Ваше имя?')
+# surname = input('Ваша фамилия?')
+# group = input('Ваша группа?')
+# print(f'Привет, {surname} {name} из группы {group}!')
+# email = input('Введите свою электронную почту?')
+# print(surname[:5].lower() + name[:5].lower()*2 + email[:5].lower()*3)
+
+random_num = randint(2, 9)
