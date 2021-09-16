530173fb69101a2274b007245dfe439fa2f6edee
Vasiliy <vasia22012003@gmail.com>
2021-09-16 23:08:20 +0500

    lab2-5 is done

--- lab3-5.py
+++ lab3-5.py
@@ -1,9 +1,17 @@
 from random import randint
-# name = input('Ваше имя?')
-# surname = input('Ваша фамилия?')
-# group = input('Ваша группа?')
-# print(f'Привет, {surname} {name} из группы {group}!')
-# email = input('Введите свою электронную почту?')
-# print(surname[:5].lower() + name[:5].lower()*2 + email[:5].lower()*3)
 
-random_num = randint(2, 9)
+name = input('Ваше имя?')
+surname = input('Ваша фамилия?')
+group = input('Ваша группа?')
+print(f'Привет, {surname} {name} из группы {group}!')
+email = input('Введите свою электронную почту?')
+print(surname[:5].lower() + name[:5].lower() * 2 + email[:5].lower() * 3)
+
+random_num = 2
+
+print(random_num * '   _~_    ')
+print(random_num * '  (o o)   ')
+print(random_num * ' /  V  \  ')
+print(random_num * '/(  _  )\ ')
+print(random_num * '  ^^ ^^   ')
+
