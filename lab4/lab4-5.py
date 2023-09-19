3994bd7e8aa018f7c7639aee770de862d87fcae1
Vasiliy Khlopunov <59390118+Hlopunoff@users.noreply.github.com>
2023-09-19 23:55:52 +0500

    Delete lab4 directory

--- lab4/lab4-5.py
+++
@@ -1,9 +0,0 @@
-soldiers = {}
-
-for i in range(5):
-    information = input().split(' ')
-    soldiers['{0} {1} {2}'.format(*information[:3])] = '{0} {1}'.format(*information[3:])
-
-print('Фамилия\t\t\tИмя\t\t\t\tОтчество\t\tГод рождения\tЗаболевание')
-for key in soldiers.keys():
-    print('{:<15} {:<15} {:<15} {:<15} {:<15}'.format(*key.split(' '), *soldiers[key].split(' ')))
